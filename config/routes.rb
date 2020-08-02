42b4f8dda623aebbbe5c4257d58dfa57bd9b6d92
Luke Kingsley <lukekingsley@lukes-mbp.attlocal.net>
2020-08-02 15:49:19 -0700

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # get 'orders/gift', :to => 'orders#gift'
   resources :products, only: [ :index, :show ]
   resources :orders, only: [ :new, :create, :show ]
 
-  
   root to: "products#index"
 end
 
